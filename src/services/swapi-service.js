1436c6f02e5d1e14643204f378af817fcfaab496
Artem <artem.xolod.90@gmail.com>
2021-06-18 16:07:04 +0300

    Реализация начальных компонентов

---
+++ src/services/swapi-service.js
@@ -0,0 +1,46 @@
+export default class SwapiService {
+
+    _apiBase = 'https://swapi.py4e.com/api';
+
+    async getResource(url) {
+        const res = await fetch(`${this._apiBase}${url}`);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, received ${res.status}`)
+        }
+
+        return await res.json();
+    }
+
+    async getAllPeople() {
+        const res = await this.getResource(`/people/`);
+        return res.results;
+    }
+
+    getPerson(id) {
+        return this.getResource(`/people/${id}/`)
+    }
+
+    async getAllPlanets() {
+        const res = await this.getResource(`/planets/`);
+        return res.result;
+    }
+
+    getPlanets(id) {
+        return this.getResource(`/planets/${id}/`)
+    }
+
+    async getAllStarships() {
+        const res = await this.getResource(`/starships/`);
+        return res.result;
+    }
+
+    getStarships(id) {
+        return this.getResource(`/starships/${id}/`)
+    }
+}
+
+const swapi = new SwapiService();
+swapi.getAllPeople().then((body) => {
+    console.log(body)
+})
