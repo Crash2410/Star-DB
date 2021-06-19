5e5decae9013b284b458e6430521679102c933d8
Artem <artem.xolod.90@gmail.com>
2021-06-19 18:06:28 +0300

    использование componentDidUpdate() на практике

--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -1,32 +1,77 @@
 import React, { Component } from 'react';
-
+import SwapiService from '../../services/swapi-service';
+import Spinner from '../spinner/index';
 import './person-details.css';
 
 export default class PersonDetails extends Component {
+  swapiService = new SwapiService();
+  state = {
+    person: null,
+  }
 
-  render() {
+  componentDidMount() {
+    this.updatePersone();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.personId !== prevProps.personId) {
+      this.updatePersone();
+    }
+  }
+
+  updatePersone() {
+    const { personId } = this.props;
+    if (!personId) {
+      return;
+    }
+    this.swapiService
+      .getPerson(personId || 1)
+      .then((person) => {
+        this.setState({
+          person
+        })
+      })
+  }
+
+  renderItems = ({ id, name, gender, birthYear, yeyColor }) => {
     return (
-      <div className="person-details card">
+      <React.Fragment>
         <img className="person-image"
-          src="https://starwars-visualguide.com/assets/img/characters/3.jpg" />
+          src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} />
 
         <div className="card-body">
-          <h4>R2-D2</h4>
+          <h4>{name}</h4>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
               <span className="term">Gender</span>
-              <span>male</span>
+              <span>{gender}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Birth Year</span>
-              <span>43</span>
+              <span>{birthYear}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Eye Color</span>
-              <span>red</span>
+              <span>{yeyColor}</span>
             </li>
           </ul>
         </div>
+      </React.Fragment>
+    );
+  }
+
+  render() {
+    const { person } = this.state;
+
+    if (!person) {
+      return <Spinner />
+    }
+   
+    const personRender = this.renderItems(person);
+
+    return (
+      <div className="person-details card">
+        {personRender}
       </div>
     )
   }
