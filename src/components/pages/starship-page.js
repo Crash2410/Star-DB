55b94cc850b63f6370e420e68ded27f894ac078c
Artem <artem.xolod.90@gmail.com>
2021-07-03 22:39:55 +0300

    Относительные пути

--- src/components/pages/starship-page.js
+++ src/components/pages/starship-page.js
@@ -7,7 +7,7 @@ import {
 const StarshipPage = ({ history }) => {
     return (
         <StarshipList onItemSelected={(itemId) => {
-            history.push(`/starships/${itemId}`);
+            history.push(itemId);
         }} />
     );
 };
